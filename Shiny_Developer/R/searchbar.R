385ff8a0df4065c84275f271e3cc159b33873eea
adevi12345 <92974092+adevi12345@users.noreply.github.com>
2022-01-18 00:41:31 +0530

    Update searchbar.R

--- Shiny_Developer/R/searchbar.R
+++ Shiny_Developer/R/searchbar.R
@@ -9,33 +9,35 @@ searchbar_ui <- function(id) {
     # Create select input to search the names of the user input
     
     selectInput(ns('search_text'),tags$h4('Search with ScientificName:'), choices = NULL, selectize= TRUE )
-    
   )
-  
-}
+ 
+  }
 # End of the User Interface Module-1
 
 # Start of the Server Module-1
 searchbar_server <- function(id) {
-  
+
   moduleServer(id, function(input, output, session) {   # Moduleserver function starts..
-    
-    input_dataset<-read.csv("./input_final.csv")
+
+    input_dataset<-read.csv("./input_final.csv")  # Load the inputdataset
     
     # Observe is to update the choices to the select input widget
-    observe(
-      updateSelectInput(
-        session,"search_text",choices = unique(c(input_dataset$scientificName,input_dataset$vernacularName)) 
-      ))  # End of the observe
+    observe({
+      choices_values<-unique(c(input_dataset$scientificName,input_dataset$vernacularName))
+      
+      updateSelectInput(          
+        session,"search_text", choices=c('',choices_values)
+      )
+      })  # End of the observe
     
     # Return the list of values from the Server Module-1
     return(
       list(
         input_search = reactive({input$search_text}),  # Return the search input reactive
-        input_dataset=reactive({input_dataset})  # Return the inputdataset reactive
-      )
+        input_dataset=reactive({input_dataset}), # Return the inputdataset reactive
+        action_button =reactive({input$view_dataset})
+)
     ) # End of the return
     
   })  # End of the Module server function
 }  # End of the Server Module-1
-
