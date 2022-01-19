e2d304e98cf7b6a22b2ed283bd06a49f393cfc01
adevi12345 <92974092+adevi12345@users.noreply.github.com>
2022-01-19 23:32:11 +0530

    Update searchbar.R

--- Shiny_Developer/R/searchbar.R
+++ Shiny_Developer/R/searchbar.R
@@ -8,7 +8,9 @@ searchbar_ui <- function(id) {
     
     # Create select input to search the names of the user input
     
-    selectInput(ns('search_text'),tags$h4('Search with ScientificName:'), choices = NULL, selectize= TRUE )
+    selectInput(ns('search_text'),tags$h4('Search with ScientificName:'), choices = NULL, selectize= TRUE ),
+    tags$hr(),
+    uiOutput(ns('numberof_choices_ui'))  # UI for choices list
   )
  
   }
@@ -18,24 +20,34 @@ searchbar_ui <- function(id) {
 searchbar_server <- function(id) {
 
   moduleServer(id, function(input, output, session) {   # Moduleserver function starts..
-    
-    input_dataset<-read.csv("poland_data.csv")  # Load the inputdataset
+
+    input_dataset<-read.csv("./poland_data.csv")  # Load the inputdataset
     
     # Observe is to update the choices to the select input widget
     observe({
+      
       choices_values<-unique(c(input_dataset$scientificName,input_dataset$vernacularName))
+      
+      # Update the selectinput session
       
       updateSelectInput(          
         session,"search_text", choices=c('',choices_values)
       )
+     # Render the output of the choice list
+       output$numberof_choices_ui<-renderUI({
+        valueBox(width=125,length(choices_values), "Number of choices at the Searchbar", icon = icon("list"))
+        
+      })  # End of the Render function
+       
       })  # End of the observe
+    
+    
     
     # Return the list of values from the Server Module-1
     return(
       list(
         input_search = reactive({input$search_text}),  # Return the search input reactive
-        input_dataset=reactive({input_dataset}), # Return the inputdataset reactive
-        action_button =reactive({input$view_dataset})
+        input_dataset=reactive({input_dataset}) # Return the inputdataset reactive
 )
     ) # End of the return
     
